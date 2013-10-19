ed6812ef0479f8c1759e1ec357083bccbac32b67
Ian Ross <ian@skybluetrades.net>
2013-10-19 14:35:57 +0200

    Beginnings of monadic "name supply" for module rendering

--- Language/ModuleDSL/Render.hs
+++ Language/ModuleDSL/Render.hs
@@ -11,11 +11,9 @@ import qualified Data.Aeson as A
 import Data.Attoparsec.Number (Number(..))
 import Text.Blaze
 import Text.Julius
+import Control.Monad.Trans.State
 
 import Language.ModuleDSL.Syntax
-
-class Render a where
-  render :: a -> (Markup, JavascriptUrl url)
 
 instance ToMarkup Name where
   toMarkup (Name n) = toMarkup n
@@ -135,101 +133,153 @@ instance ToJavascript Expr where
     "{" <> (mconcat $ intersperse "," $
             map (\(k, v) -> toJavascript k <> "=" <> toJavascript v) fs) <> "}"
 
-instance Render Module where
-  render (Module _name _opts body) = mconcat $ map render body
+data RenderState = RenderState { textIdx :: Integer
+                               , utilIdx :: Integer
+                               , locals :: [Name] } deriving (Eq, Show)
 
-instance Render TopLevel where
-  render (SurveyPage name _opts body) =
-    let (qs, ss) = mconcat $ map render body
-    in ([shamlet|
-         <h4>
-           Survey page: #{name}
-         ^{qs}
-        |],
-        [julius|console.log("Survey page: #{name}");|] <> ss)
-  render (Specialisation _name _params _body) =
-    (toMarkup ("Specialisation..." :: String), mempty)
-  render (Function name params body) =
-    (mempty, [julius|
+initRenderState :: RenderState
+initRenderState = RenderState 0 0 []
+
+nextTextIdx :: State RenderState Integer
+nextTextIdx = do
+  tidx <- gets textIdx
+  modify (\s -> s { textIdx = tidx + 1 })
+  return tidx
+
+nextUtilIdx :: State RenderState Integer
+nextUtilIdx = do
+  uidx <- gets utilIdx
+  modify (\s -> s { utilIdx = uidx + 1 })
+  return uidx
+
+
+render :: RunRender a => a -> (Markup, JavascriptUrl url)
+render v = evalState (runRender v) initRenderState
+
+type Render url = State RenderState (Markup, JavascriptUrl url)
+
+class RunRender a where
+  runRender :: a -> Render url
+
+
+instance RunRender Module where
+  runRender (Module _name _opts body) = mconcat <$> mapM runRender body
+
+
+instance RunRender TopLevel where runRender = renderTopLevel
+
+renderTopLevel :: TopLevel -> Render url
+
+renderTopLevel (SurveyPage name _opts body) = do
+  (qs, ss) <- mconcat <$> mapM runRender body
+  return ([shamlet|
+  <h4>
+    Survey page: #{name}
+  ^{qs}
+          |],
+          [julius|console.log("Survey page: #{name}");|] <> ss)
+
+renderTopLevel (Specialisation _name _params _body) =
+  return (toMarkup ("Specialisation..." :: String), mempty)
+
+renderTopLevel (Function name params body) =
+  return (mempty,
+          [julius|
   sc.#{name} = function(#{params}) {
     return #{body};
   };
-             |])
+          |])
 
--- RENDER NEEDS TO HAVE SOME SORT OF MONADIC NAME SUPPLY...
 
-instance Render Question where
-  render (NumericQuestion name t os) =
-    let LitExpr (Integer mn) = lookupOpt "min" os (LitExpr $ Integer 0)
-        LitExpr (Integer mx) = lookupOpt "max" os (LitExpr $ Integer 100)
-        initVal = (mn + mx) `div` 2
-    in ([shamlet|
-         <div .question>
-           <div .question-text>
-             #{t}
-           <div>
-             <input type="range" min=#{mn} max=#{mx}
-                         ng-model="results['#{name}']">
-             <span>
-               {{results['#{name}']}}
-        |],
-        [julius|
-console.log("Numeric question: #{name}");
-sc.results['#{name}'] = #{toJSON initVal};
-sc.postprocess['#{name}'] = postprocessor.numeric;
-|])
-  render (ChoiceQuestion name qt _os cs) =
-    ([shamlet|
-      <div .question>
-        <div .question-text>
+instance RunRender Question where runRender = renderQuestion
+
+renderQuestion :: Question -> Render url
+
+renderQuestion (NumericQuestion name qt os) = do
+  let LitExpr (Integer mn) = lookupOpt "min" os (LitExpr $ Integer 0)
+      LitExpr (Integer mx) = lookupOpt "max" os (LitExpr $ Integer 100)
+      initVal = (mn + mx) `div` 2
+  tidx <- nextTextIdx
+  return ([shamlet|
+  <div .question>
+    <div .question-text>
+      {{text[#{tidx}]}}
+    <div>
+      <input type="range" min=#{mn} max=#{mx}
+                  ng-model="results['#{name}']">
+      <span>
+        {{results['#{name}']}}
+          |],
+          [julius|
+  console.log("Numeric question: #{name}");
+  sc.text[#{Number (I tidx)}] = #{String qt};
+  sc.results['#{name}'] = #{toJSON initVal};
+  sc.postprocess['#{name}'] = postprocessor.numeric;
+          |])
+
+renderQuestion (ChoiceQuestion name qt _os cs) = do
+  tidx <- nextTextIdx
+  return ([shamlet|
+  <div .question>
+    <div .question-text>
+      <span>
+        {{text[#{tidx}]}}
+      $forall Choice ct (LitExpr v) <- cs
+        <label .radio>
           <span>
-            #{qt}
-          $forall Choice ct (LitExpr v) <- cs
-            <label .radio>
-              <span>
-                #{ct}
-              <input type="radio" ng-model="results['#{name}']"
-                     value=#{toMarkup v} name=#{"radio_" <> name}>
-     |],
-     [julius|
-console.log("Choice question: #{name}");
-sc.results['#{name}'] = null;
-|])
-  render (DropdownQuestion name qt _os cs) =
-    ([shamlet|
-      <div .question>
-        <div .question-text>
-          <span>
-            #{qt}
-          <select ng-model="results['#{name}']"
-                  ng-options="c.label for c in choices">
-     |],
-     [julius|
-console.log("Dropdown question: #{name}");
-sc.choices = #{cs};
-sc.results['#{name}'] = sc.choices[0];
-sc.postprocess['#{name}'] = postprocessor.choices;
-|])
-  render (TextEntryQuestion name qt _os) =
-    ([shamlet|
-      <div .question>
-        <div .question-text>
-          <span>
-            #{qt}
-          <input type="text" ng-model="results['#{name}']">
-     |],
-     [julius|
-console.log("Text entry question: #{name}");
-sc.results['#{name}'] = '';
-|])
-  render (TextDisplay qt _os) =
-    ([shamlet|
-      <div .question>
-        <div .question-text>
-          <span>
-            {{text[0]}}
-     |],
-     [julius|
-console.log("Text display...");
-sc.text[0] = #{qt};
-|])
+            #{ct}
+          <input type="radio" ng-model="results['#{name}']"
+                 value=#{toMarkup v} name=#{"radio_" <> name}>
+          |],
+          [julius|
+  console.log("Choice question: #{name}");
+  sc.text[#{Number (I tidx)}] = #{String qt};
+  sc.results['#{name}'] = null;
+          |])
+
+renderQuestion (DropdownQuestion name qt _os cs) = do
+  tidx <- nextTextIdx
+  cidx <- nextUtilIdx
+  return ([shamlet|
+  <div .question>
+    <div .question-text>
+      <span>
+        {{text[#{tidx}]}}
+      <select ng-model="results['#{name}']"
+              ng-options="c.label for c in utils[#{cidx}]">
+          |],
+          [julius|
+  console.log("Dropdown question: #{name}");
+  sc.text[#{Number (I tidx)}] = #{String qt};
+  sc.utils[#{Number (I cidx)}] = #{cs};
+  sc.results['#{name}'] = sc.utils[#{Number (I cidx)}];
+  sc.postprocess['#{name}'] = postprocessor.choices;
+          |])
+
+renderQuestion (TextEntryQuestion name qt _os) = do
+  tidx <- nextTextIdx
+  return ([shamlet|
+  <div .question>
+    <div .question-text>
+      <span>
+        {{text[#{tidx}]}}
+      <input type="text" ng-model="results['#{name}']">
+          |],
+          [julius|
+  console.log("Text entry question: #{name}");
+  sc.text[#{Number (I tidx)}] = #{String qt};
+  sc.results['#{name}'] = '';
+          |])
+
+renderQuestion (TextDisplay qt _os) = do
+  tidx <- nextTextIdx
+  return ([shamlet|
+  <div .question>
+    <div .question-text>
+      <span>
+        {{text[#{tidx}]}}
+          |],
+          [julius|
+  console.log("Text display...");
+  sc.text[#{Number (I tidx)}] = #{qt};
+          |])
